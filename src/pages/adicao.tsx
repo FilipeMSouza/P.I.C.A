c9bd17d3ca9ea09a2c1bb8c709dab76913446995
FilipeMSouza <filipe.souza1906@gmail.com>
2021-11-11 20:18:35 -0300

    feature/body_page | Corpo base
    - Botões de ideia primaria adicionados

--- src/pages/adicao.tsx
+++ src/pages/adicao.tsx
@@ -1,14 +1,40 @@
-import { Container, Content } from "./style"
+import { Container, RightContent, LeftContent } from "./style"
+import Logo from "../assets/placeholders/lena_gray_512.jpeg";
 
 
- 
+
+
 export default function Adicao() {
-    
+
+
+
     return (
         <Container>
             <h1>Operação de Adição entre duas imagens</h1>
-            <Content>
-            </Content>
+            <p>Essa imagem soma duas imagens resultando em outra</p>
+            <RightContent>
+                <div>
+                    <img className="imgSoma" src={Logo} alt="logo" />
+                    <button onClick={() => console.log("Clicked")}>Somar às outras imagens</button>
+                </div>
+                <div>
+                    <img className="imgSoma" src={Logo} alt="logo" />
+                    <button onClick={() => console.log("Clicked")}>Somar às outras imagens</button>
+                </div>
+                <div>
+                    <img className="imgSoma" src={Logo} alt="logo" />
+                    <button onClick={() => console.log("Clicked")}>Somar às outras imagens</button>
+                </div>
+                <div>
+                    <img className="imgSoma" src={Logo} alt="logo" />
+                    <button onClick={() => console.log("Clicked")}>Somar às outras imagens</button>
+                </div>
+            </RightContent>
+
+            <LeftContent>
+                <img className="imgSoma" src={Logo} alt="logo" />
+            </LeftContent>
+
         </Container>
     );
 }
--- src/pages/style.ts
+++ src/pages/style.ts
@@ -1,13 +1,59 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
-    
-    display: flex;
+    display:flex;
     flex-direction: column;
-    align-items: center;
-    justify-content: center;
     margin-top: 2rem;
+    h1,p {
+        margin-left: auto;
+        margin-right: auto;
+    }
     
 `;
-export const Content= styled.div`
+export const RightContent= styled.div`
+    display: flex;
+    align-items: flex-start;
+    flex-direction: column;
+    
+    div{ 
+        display: flex;
+        flex-direction: horizontal;
+    }
+    img{
+        margin-left: 2rem;
+        padding: 0.75rem;
+        &.imgSoma{
+            height: 4rem;
+            width:  4rem;
+        }
+    }
+
+    button{
+        margin-top: 1.25rem;
+        height: 3rem;
+        background: var(--Dark-brown);
+        padding: 0.5rem 0.5rem;
+        border-radius: 0.25rem;
+        color: var(--light-brown);
+        border-color: var(--Dark-brown);
+        box-shadow: none;
+    
+    }
+
+`;
+
+export const LeftContent= styled.div`
+    display: flex;
+    align-items: flex-end;
+    flex-direction: column;
+    margin-top: -21rem;
+    
+    img{
+        margin-right: 5rem;
+        padding: 0.75rem;
+        &.imgSoma{
+            height: 20rem;
+            width:  20rem;
+        }
+    }
 `;
