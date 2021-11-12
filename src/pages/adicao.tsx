5dbbae1ad49df9a5b1505bced81f2fb04e7f78c6
FilipeMSouza <filipe.souza1906@gmail.com>
2021-11-12 18:23:46 -0300

    feature/body_page | Estado de ojetos
    - Os estados que são um possibilidade para carregar os resultados de
      cada operação estão inseridos

--- src/pages/adicao.tsx
+++ src/pages/adicao.tsx
@@ -1,23 +1,49 @@
 import { Container, Content, AuxContainer, ButtonContent, Result } from "./style"
 import LogoA from "../assets/placeholders/lena_gray_512.jpeg";
+import {useState} from 'react';
 
 
 
 export default function Adicao() {
 
+    const [op1,setOp1] = useState("off")
+    const [op2,setOp2] = useState("off")
+    const [op3,setOp3] = useState("off")
+    const [op4,setOp4] = useState("off")
+
     function handleClick(param: number){
         switch(param){
             case 1:
-                console.log(1);
+                console.log(1)
+                setOp1("on");
+                setOp2("off");
+                setOp3("off");
+                setOp4("off");
+                console.log(op1,op2,op3,op4);
                 break;
             case 2:
                 console.log(2);
+                setOp2("on");
+                setOp1("off");
+                setOp3("off");
+                setOp4("off");
+                console.log(op1,op2,op3,op4);
                 break;
             case 3:
                 console.log(3);
+                setOp3("on");
+                setOp2("off");
+                setOp1("off");
+                setOp4("off");
+                console.log(op1,op2,op3,op4);
                 break;  
             case 4:
                 console.log(4);
+                setOp4("on");
+                setOp2("off");
+                setOp3("off");
+                setOp1("off");
+                console.log(op1,op2,op3,op4);
                 break;
             default:
                 break;
