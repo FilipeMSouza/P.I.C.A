46f4120d00bd75faeafa03fc80b4d7678080cf0d
matheuszinn <theustloz@gmail.com>
2021-11-14 13:58:17 -0300

    feature/sum_images | Melhorando normalização

--- src/modules/image-processing.ts
+++ src/modules/image-processing.ts
@@ -1,26 +1,29 @@
 import { Image } from "image-js";
+import { resizeImg, readImageAsBase64, normalizeImageData } from "./utils";
 
 let sumTwoImages = async (
   image1Base64: Promise<string>,
   image2Base64: Promise<string>
-): Promise<string> => {
+) => {
   let firstImageData = await image1Base64.then(
-    async (data) => await Image.load(data).then((image) => image.grey().data)
+    async (data) =>
+      await Image.load(data).then((image) => resizeImg(image).grey().data)
   );
   let secondImageData = await image2Base64.then(
-    async (data) => await Image.load(data).then((image) => image.grey().data)
+    async (data) =>
+      await Image.load(data).then((image) => resizeImg(image).grey().data)
   );
 
-  let newImageData = [];
+  let newImageData: number[] = new Array(firstImageData.length);
 
-  for (let i = 0; i <= firstImageData.length - 1; i++) {
-    newImageData.push((firstImageData[i] + secondImageData[i]) as number);
+  for (let i in firstImageData) {
+    newImageData[i] = (firstImageData[i] + secondImageData[i]) as number;
   }
 
   let newImage = new Image({
     width: Math.sqrt(firstImageData.length),
     height: Math.sqrt(firstImageData.length),
-    data: newImageData.map((value: number): number => Math.floor(value / 2)),
+    data: normalizeImageData(newImageData),
     kind: "GREY",
   });
 
