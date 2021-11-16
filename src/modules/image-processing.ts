1ffa656f70542912f00b328594aa9fd571e1806e
Victor Hugo C. Gomes <vhclaudiogomes@gmail.com>
2021-11-15 23:48:07 -0300

    subimage | adicionado função para subtração

--- src/modules/image-processing.ts
+++ src/modules/image-processing.ts
@@ -69,3 +69,24 @@ let multiplyTwoImages = async (
 
   return newImage.toDataURL();
 };
+
+let subtractTwoImages = async (
+  image1Base64: string,
+  image2Base64: string
+) => {
+  let firstImageData = await Image.load(image1Base64).then(image => resizeImg(image).grey().data)
+  let secondImageData = await Image.load(image2Base64).then(image => resizeImg(image).grey().data)
+
+  let newImageData: number[] = new Array(firstImageData.length);
+  for (let i in firstImageData){
+    newImageData[i] = (firstImageData[i] - secondImageData[i]) as number
+  }
+
+  let newImage: Image = new Image({
+    width: Math.sqrt(firstImageData.length),
+    height: Math.sqrt(firstImageData.length),
+    data: normalizeImageData(newImageData),
+    kind: ImageKind.GREY,
+  })
+  return newImage.toDataURL();
+}
