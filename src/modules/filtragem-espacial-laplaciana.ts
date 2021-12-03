9d9233084ea09e7e0d937ef153445f20388ecab1
Matheus Alves <theustloz@gmail.com>
2021-12-03 02:55:23 -0300

    feature/filtro_laplace :: Detector de sobel e roberts

--- src/modules/filtragem-espacial-laplaciana.ts
+++ src/modules/filtragem-espacial-laplaciana.ts
@@ -13,6 +13,13 @@ class FiltrosLaplace {
     static FILTRO_LAPLACE_D = [-1, -1, -1, -1, 8, -1, -1, -1, -1];
 }
 
+class RobertSobelsOptions {
+    static SobelHorizontal = [-1, -2, -1, 0, 0, 0, 1, 2, 1];
+    static SobelVertical = [-1, 0, 1, -2, 0, 2, -1, 0, 1];
+    static RobertsMx = [1, 0, 0, -1];
+    static RobertsMy = [0, 1, -1, 0];
+}
+
 const ehBorda = (i: number, j: number, img: Image): boolean => {
     return i === 0 || j === 0 || i === img.height - 1 || j === img.width - 1;
 };
@@ -137,7 +144,7 @@ const unsharp_mask_highboost = async (
 
     let FILTRO_UNSHARP_MASK = [0, -1, 0, -1, 4 + k_value, -1, 0, -1, 0];
 
-    k_value > 1 ?? console.log('APLICANDO HIGH BOOST!!!!!!')
+    k_value > 1 ?? console.log("APLICANDO HIGH BOOST!!!!!!");
 
     for (let x = 0; x < newImage.height; x++) {
         for (let y = 0; y < newImage.width; y++) {
@@ -178,8 +185,79 @@ const unsharp_mask_highboost = async (
     return newImage.toDataURL();
 };
 
-readImageAsBase64("client/src/assets/placeholders/lena.jpeg").then(path =>
-    unsharp_mask_highboost(path,1.5).then(b =>
-        Image.load(b).then(i => i.save("unsharp.png"))
-    )
-);
+const detector_sobel = async (imageBase64: string, filtro: number[]) => {
+    let oImage: Image = resizeImg(await Image.load(imageBase64)).grey();
+
+    let newImage: Image = new Image({
+        width: oImage.width,
+        height: oImage.height,
+        kind: ImageKind.GREY,
+    });
+
+    // Detector Sobbels porque é mais fácil primeiro
+
+    let oImageMatrix: number[][] = getMatrixFromImage(oImage);
+
+    for (let x = 0; x < oImage.height; x++) {
+        for (let y = 0; y < oImage.width; y++) {
+            //Usando método de replicação de borda
+
+            if (!ehBorda(x, y, newImage)) {
+                let pixelsVizinhanca = getVizinhanca(x, y, oImageMatrix);
+
+                let newPixelValue = pixelsVizinhanca.reduce(
+                    (result, pl, idx) => result + pl * filtro[idx],
+                    0
+                );
+
+                newImage.setPixelXY(y, x, [255 - clamp(newPixelValue)]);
+            } else {
+                newImage.setPixelXY(y, x, [oImageMatrix[x][y]]);
+            }
+        }
+    }
+
+    return newImage.toDataURL();
+};
+
+const detector_roberts = async (imageBase64: string) => {
+    let oImage: Image = resizeImg(await Image.load(imageBase64)).grey();
+    let newImage: Image = new Image({
+        width: oImage.width,
+        height: oImage.height,
+        kind: ImageKind.GREY,
+    });
+
+    const oImageMatrix: number[][] = getMatrixFromImage(oImage);
+
+    for (let x = 0; x < newImage.height; x++) {
+        for (let y = 0; y < newImage.width; y++) {
+            if (ehBorda(x, y, oImage)) {
+                newImage.setPixelXY(y, x, [oImageMatrix[x][y]]);
+            } else {
+                let PIXELlIST: number[] = [
+                    oImageMatrix[x][y],
+                    oImageMatrix[x][y + 1],
+                    oImageMatrix[x + 1][y],
+                    oImageMatrix[x + 1][y + 1],
+                ];
+
+                let Gx = PIXELlIST.reduce(
+                    (result, a, b) =>
+                        result + a * RobertSobelsOptions.RobertsMx[b],
+                    0
+                );
+                let Gy = PIXELlIST.reduce(
+                    (r, a, b) => r + a * RobertSobelsOptions.RobertsMy[b],
+                    0
+                );
+
+                let newPixelValue = Math.abs(Gx) + Math.abs(Gy);
+
+                newImage.setPixelXY(y, x, [newPixelValue]);
+            }
+        }
+    }
+
+    return newImage.toDataURL();
+};
