99cf139d3bd2ec5f7221e2eb47064c478a3783da
Matheus Alves <theustloz@gmail.com>
2021-12-03 18:20:14 -0300

    feature/filtro_laplace :: adicionadas as rotas

--- src/index.ts
+++ src/index.ts
@@ -1,159 +1,210 @@
 import express, { NextFunction } from "express";
 
 import {
-  multiplyTwoImages,
-  sumTwoImages,
-  divTwoImages,
-  subTwoImages,
+    multiplyTwoImages,
+    sumTwoImages,
+    divTwoImages,
+    subTwoImages,
 } from "../src/modules/operacoes-aritmeticas";
 
 import {
-  reflexao,
-  translacao,
-  escala,
-  rotacao,
+    reflexao,
+    translacao,
+    escala,
+    rotacao,
 } from "../src/modules/transformacoes-geometricas";
 
 import {
-  histograma,
-  histogramaEqualizado,
-  histogramaNormalizado,
-  equalizarImagem,
-  contrastStretching,
+    histograma,
+    histogramaEqualizado,
+    histogramaNormalizado,
+    equalizarImagem,
+    contrastStretching,
 } from "../src/modules/histograma";
 
 import {
-  aplicarFiltroSmoothing,
-  TipoBorda,
+    aplicarFiltroSmoothing,
+    TipoBorda,
 } from "../src/modules/filtro-smoothing";
+
+import {
+    laplaciano,
+    laplacianoDoGaussiano,
+    unsharp_mask_highboost,
+    detector_sobel,
+    detector_roberts
+} from "../src/modules/filtragem-espacial-laplaciana";
+
 import Mascara from "./modules/Mascara";
 
 const PORT: number = 8048;
 const app = express();
 
 app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  next();
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept"
+    );
+    next();
 });
 
 app.get("/sum/:image1&:image2", async (req: any, res) => {
-  let imagePath1: string = req.params["image1"];
-  let imagePath2: string = req.params["image2"];
+    let imagePath1: string = req.params["image1"];
+    let imagePath2: string = req.params["image2"];
 
-  const value = await sumTwoImages(imagePath1, imagePath2);
-  return res.status(200).json({ data: value });
+    const value = await sumTwoImages(imagePath1, imagePath2);
+    return res.status(200).json({ data: value });
 });
 
 app.get("/multiply/:image1&:image2", async (req: any, res) => {
-  let imagePath1: string = req.params["image1"];
-  let imagePath2: string = req.params["image2"];
+    let imagePath1: string = req.params["image1"];
+    let imagePath2: string = req.params["image2"];
 
-  const value = await multiplyTwoImages(imagePath1, imagePath2);
-  return res.status(200).json({ data: value });
+    const value = await multiplyTwoImages(imagePath1, imagePath2);
+    return res.status(200).json({ data: value });
 });
 
 app.get("/subtract/:image1&:image2", async (req: any, res) => {
-  let imagePath1: string = req.params["image1"];
-  let imagePath2: string = req.params["image2"];
+    let imagePath1: string = req.params["image1"];
+    let imagePath2: string = req.params["image2"];
 
-  const value = await subTwoImages(imagePath1, imagePath2);
-  return res.status(200).json({ data: value });
+    const value = await subTwoImages(imagePath1, imagePath2);
+    return res.status(200).json({ data: value });
 });
 
 app.get("/divide/:image1&:image2", async (req: any, res) => {
-  let imagePath1: string = req.params["image1"];
-  let imagePath2: string = req.params["image2"];
+    let imagePath1: string = req.params["image1"];
+    let imagePath2: string = req.params["image2"];
 
-  const value = await divTwoImages(imagePath1, imagePath2);
-  return res.status(200).json({ data: value });
+    const value = await divTwoImages(imagePath1, imagePath2);
+    return res.status(200).json({ data: value });
 });
 
 app.get("/reflexao/:image", async (req: any, res) => {
-  let imagePath: string = req.params["image"];
-  const value = await reflexao(imagePath);
-  return res.status(200).json({ data: value });
+    let imagePath: string = req.params["image"];
+    const value = await reflexao(imagePath);
+    return res.status(200).json({ data: value });
 });
 
 app.get("/escala/:image&:Sx&:Sy", (req: any, res) => {
-  let imagePath: string = req.params["image"];
-  let Sx: number = req.params["Sx"];
-  let Sy: number = req.params["Sy"];
+    let imagePath: string = req.params["image"];
+    let Sx: number = req.params["Sx"];
+    let Sy: number = req.params["Sy"];
 
-  return escala(imagePath, Sx, Sy).then((value) =>
-    res.status(200).json({ data: value })
-  );
+    return escala(imagePath, Sx, Sy).then(value =>
+        res.status(200).json({ data: value })
+    );
 });
 
 app.get("/translacao/:image&:xOff&:yOff", async (req: any, res) => {
-  let imagePath: string = req.params["image"];
-  let xOffset: number = req.params["xOff"];
-  let yOffset: number = req.params["yOff"];
+    let imagePath: string = req.params["image"];
+    let xOffset: number = req.params["xOff"];
+    let yOffset: number = req.params["yOff"];
 
-  const value = await translacao(imagePath, xOffset, yOffset);
-  return res.status(200).json({ data: value });
+    const value = await translacao(imagePath, xOffset, yOffset);
+    return res.status(200).json({ data: value });
 });
 
 app.get("/rotacao/:image&:theta", async (req: any, res) => {
-  let imagePath: string = req.params["image"];
-  let theta: number = req.params["theta"];
+    let imagePath: string = req.params["image"];
+    let theta: number = req.params["theta"];
 
-  const value = await rotacao(imagePath, theta);
-  return res.status(200).json({ data: value });
+    const value = await rotacao(imagePath, theta);
+    return res.status(200).json({ data: value });
 });
 
 app.get("/histograma/:tipo&:imagePath", async (req: any, res) => {
-  let tipo: string = req.params["tipo"];
-  let imagePath: string = req.params["imagePath"];
+    let tipo: string = req.params["tipo"];
+    let imagePath: string = req.params["imagePath"];
 
-  let data;
+    let data;
 
-  switch (tipo) {
-    case "Normalizado":
-      data = await histogramaNormalizado(imagePath, true);
-      break;
-    case "Equalizado":
-      data = await histogramaEqualizado(imagePath);
-      break;
-    default:
-      data = await histograma(imagePath, true);
-  }
-  return res.status(200).json({ data: data });
+    switch (tipo) {
+        case "Normalizado":
+            data = await histogramaNormalizado(imagePath, true);
+            break;
+        case "Equalizado":
+            data = await histogramaEqualizado(imagePath);
+            break;
+        default:
+            data = await histograma(imagePath, true);
+    }
+    return res.status(200).json({ data: data });
 });
 
 app.get("/imagemEqualizada/:imagePath", async (req: any, res) => {
-  let imagePath: string = req.params["imagePath"];
+    let imagePath: string = req.params["imagePath"];
 
-  const result = await equalizarImagem(imagePath);
-  return res.status(200).json({ data: result });
+    const result = await equalizarImagem(imagePath);
+    return res.status(200).json({ data: result });
 });
 
 app.get("/contrastStretching/:imagePath", async (req: any, res) => {
-  let imagePath: string = req.params["imagePath"];
+    let imagePath: string = req.params["imagePath"];
 
-  const result = await contrastStretching(imagePath);
-  return res.status(200).json({ data: result });
+    const result = await contrastStretching(imagePath);
+    return res.status(200).json({ data: result });
 });
 
 app.get("/smoothing/:imagePath&:mascara&:tipoBorda", async (req: any, res) => {
-  let imagePath: string = req.params["imagePath"];
-  let tipoBorda: TipoBorda = parseInt(req.params["tipoBorda"])
+    let imagePath: string = req.params["imagePath"];
+    let tipoBorda: TipoBorda = parseInt(req.params["tipoBorda"]);
 
-  let mascara = new Mascara(
-    eval(req.params["mascara"]) as number[][],
-    tipoBorda
-  );
+    let mascara = new Mascara(
+        eval(req.params["mascara"]) as number[][],
+        tipoBorda
+    );
 
-  const result = await aplicarFiltroSmoothing(imagePath, mascara);
-  return res.status(200).json({ data: result });
+    const result = await aplicarFiltroSmoothing(imagePath, mascara);
+    return res.status(200).json({ data: result });
 });
+
+app.get("/laplaciano/:imagePath&:filtro&:divByNine", async (req: any, res) => {
+    let imagePath: string = req.params["imagePath"];
+    let filtro: number[] = eval(req.params["filtro"]) as number[];
+    let divideByNine: boolean = req.params["divByNine"];
+
+    let result: string = await laplaciano(imagePath, filtro, divideByNine);
+
+    return res.status(200).json({ data: result });
+});
+
+app.get("/LoG/:imagePath&:filtro", async (req: any, res) => {
+    let imagePath: string = req.params["imagePath"];
+    let filtro: boolean = req.params["filtro"];
+
+    let result = await laplacianoDoGaussiano(imagePath, filtro);
+    return res.status(200).json({
+        data: result,
+    });
+});
+
+app.get("/unsharp_mask_highboost/:imagePath&:k", async (req: any, res) => {
+    let imagePath: string = req.params["imagePath"];
+    let k: number = req.params["k"];
+
+    let result = await unsharp_mask_highboost(imagePath, k);
+    return res.status(200).json({ data: result });
+});
+
+app.get("/detectorSobel/:imagePath", async (req:any, res) =>{
+  let imagePath: string = req.params['imagePath']
+
+  let result = await detector_sobel(imagePath)
+
+  return res.status(200).json({data: result})
+})
+
+app.get('/detectorRoberts/:imagePath', async (req:any, res) => {
+  let imagePath:string = req.params['imagePath']
+
+  let result = await detector_roberts(imagePath)
+
+  return res.status(200).json({data: result})
+})
 
 app.listen(PORT, () => {
-  console.log(`ඞ Server iniciado!!!ඞ`);
-  console.log(`ඞ Acesse em: http://localhost:${PORT} ඞ`);
+    console.log(`ඞ Server iniciado!!!ඞ`);
+    console.log(`ඞ Acesse em: http://localhost:${PORT} ඞ`);
 });
-
-// TODO: Todas as rotas são iguais, fazer uma rota que recebe a operação como parâmetro!!!
