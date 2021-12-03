c9b3eb348c9a7c85beb3d8fef50b2e2404b8db57
Matheus Alves <theustloz@gmail.com>
2021-12-03 18:19:30 -0300

    feature/filtro_laplace :: modificado algumas funçoes

--- src/modules/filtragem-espacial-laplaciana.ts
+++ src/modules/filtragem-espacial-laplaciana.ts
@@ -1,23 +1,11 @@
 import Image from "image-js";
-import {
-    readImageAsBase64,
-    resizeImg,
-    ImageKind,
-    getMatrixFromImage,
-} from "./utils";
+import { resizeImg, ImageKind, getMatrixFromImage } from "./utils";
 
 class FiltrosLaplace {
     static FILTRO_LAPLACE_A = [0, 1, 0, 1, -4, 1, 0, 1, 0];
     static FILTRO_LAPLACE_B = [1, 1, 1, 1, -8, 1, 1, 1, 1];
     static FILTRO_LAPLACE_C = [0, -1, 0, -1, 4, -1, 0, -1, 0];
     static FILTRO_LAPLACE_D = [-1, -1, -1, -1, 8, -1, -1, -1, -1];
-}
-
-class RobertSobelsOptions {
-    static SobelHorizontal = [-1, -2, -1, 0, 0, 0, 1, 2, 1];
-    static SobelVertical = [-1, 0, 1, -2, 0, 2, -1, 0, 1];
-    static RobertsMx = [1, 0, 0, -1];
-    static RobertsMy = [0, 1, -1, 0];
 }
 
 const ehBorda = (i: number, j: number, img: Image): boolean => {
@@ -40,13 +28,24 @@ const clamp = (n: number, min: number = 0, max: number = 255) => {
     return Math.min(Math.max(n, min), max);
 };
 
-const laplacianoDoGaussiano = async (imageBase64: string) => {
-    const FILTRO_GAUSS_2 = [
+export const laplacianoDoGaussiano = async (
+    imageBase64: string,
+    filtro: boolean
+) => {
+    const FILTRO_GAUSS_1_4 = [
         0, 1, 1, 2, 2, 2, 1, 1, 0, 1, 2, 4, 5, 5, 5, 4, 2, 1, 1, 4, 5, 3, 0, 3,
         5, 4, 1, 2, 5, 3, -12, -24, -12, 3, 5, 2, 2, 5, 0, -24, -40, -24, 0, 5,
         2, 2, 5, 3, -12, -24, -12, 3, 5, 2, 1, 4, 5, 3, 0, 3, 5, 4, 1, 1, 2, 4,
         5, 5, 5, 4, 2, 1, 0, 1, 1, 2, 2, 2, 1, 1, 0,
     ];
+
+    const FILTRO_GAUS_1_0 = [
+        0, 0, 1, 0, 0, 0, 1, 2, 1, 0, 1, 2, -16, 2, 1, 0, 1, 2, 1, 0, 0, 0, 1,
+        0, 0,
+    ];
+
+    let [start, end] = filtro ? [-2, 2] : [-4, 4];
+
     let oImage: Image = resizeImg(await Image.load(imageBase64)).grey();
 
     let newImage: Image = new Image({
@@ -61,28 +60,31 @@ const laplacianoDoGaussiano = async (imageBase64: string) => {
         for (let y = 0; y < newImage.width; y++) {
             // Usando método de replicação de borda
             if (
-                x >= 4 &&
-                y >= 4 &&
-                x < newImage.height - 4 &&
-                y < newImage.height - 4
+                x >= end &&
+                y >= end &&
+                x < newImage.height - end &&
+                y < newImage.height - end
             ) {
                 let pixelsVizinhanca: number[] = [];
 
-                for (let i = -4; i <= 4; i++) {
-                    for (let j = -4; j <= 4; j++) {
+                for (let i = start; i <= end; i++) {
+                    for (let j = start; j <= end; j++) {
                         let result = oImageMatrix[x + i][y + j];
                         pixelsVizinhanca.push(result);
                     }
                 }
 
                 let newPixelValue = pixelsVizinhanca.reduce(
-                    (result, pl, idx) => result + pl * FILTRO_GAUSS_2[idx],
+                    (result, pl, idx) =>
+                        result +
+                        pl *
+                            (filtro
+                                ? FILTRO_GAUS_1_0[idx]
+                                : FILTRO_GAUSS_1_4[idx]),
                     0
                 );
 
-                newImage.setPixelXY(y, x, [
-                    clamp(Math.floor(newPixelValue) / 16),
-                ]);
+                newImage.setPixelXY(y, x, [clamp(newPixelValue)]);
             } else {
                 newImage.setPixelXY(y, x, [oImageMatrix[x][y]]);
             }
@@ -92,7 +94,7 @@ const laplacianoDoGaussiano = async (imageBase64: string) => {
     return newImage.toDataURL();
 };
 
-const laplaciano = async (
+export const laplaciano = async (
     imageBase64: string,
     filtro: number[] = FiltrosLaplace.FILTRO_LAPLACE_C,
     divByNine: boolean = false
@@ -128,7 +130,7 @@ const laplaciano = async (
     return newImage.toDataURL();
 };
 
-const unsharp_mask_highboost = async (
+export const unsharp_mask_highboost = async (
     imageBase64: string,
     k_value: number = 1
 ) => {
@@ -185,16 +187,23 @@ const unsharp_mask_highboost = async (
     return newImage.toDataURL();
 };
 
-const detector_sobel = async (imageBase64: string, filtro: number[]) => {
+export const detector_sobel = async (imageBase64: string) => {
+    let vertical = [-1, 0, 1, -2, 0, 2, -1, 0, 1];
+    let horizontal = [-1, -2, -1, 0, 0, 0, 1, 2, 1];
+
     let oImage: Image = resizeImg(await Image.load(imageBase64)).grey();
 
-    let newImage: Image = new Image({
+    let newImageVertical: Image = new Image({
         width: oImage.width,
         height: oImage.height,
         kind: ImageKind.GREY,
     });
 
-    // Detector Sobbels porque é mais fácil primeiro
+    let newImageHorizontal: Image = new Image({
+        width: oImage.width,
+        height: oImage.height,
+        kind: ImageKind.GREY,
+    });
 
     let oImageMatrix: number[][] = getMatrixFromImage(oImage);
 
@@ -202,27 +211,63 @@ const detector_sobel = async (imageBase64: string, filtro: number[]) => {
         for (let y = 0; y < oImage.width; y++) {
             //Usando método de replicação de borda
 
-            if (!ehBorda(x, y, newImage)) {
+            if (!ehBorda(x, y, newImageVertical)) {
                 let pixelsVizinhanca = getVizinhanca(x, y, oImageMatrix);
 
                 let newPixelValue = pixelsVizinhanca.reduce(
-                    (result, pl, idx) => result + pl * filtro[idx],
+                    (result, pl, idx) => result + pl * vertical[idx],
                     0
                 );
 
-                newImage.setPixelXY(y, x, [255 - clamp(newPixelValue)]);
+                newImageVertical.setPixelXY(y, x, [255 - clamp(newPixelValue)]);
             } else {
-                newImage.setPixelXY(y, x, [oImageMatrix[x][y]]);
+                newImageVertical.setPixelXY(y, x, [oImageMatrix[x][y]]);
             }
         }
     }
 
-    return newImage.toDataURL();
+    for (let x = 0; x < oImage.height; x++) {
+        for (let y = 0; y < oImage.width; y++) {
+            //Usando método de replicação de borda
+
+            if (!ehBorda(x, y, newImageHorizontal)) {
+                let pixelsVizinhanca = getVizinhanca(x, y, oImageMatrix);
+
+                let newPixelValue = pixelsVizinhanca.reduce(
+                    (result, pl, idx) => result + pl * horizontal[idx],
+                    0
+                );
+
+                newImageHorizontal.setPixelXY(y, x, [
+                    255 - clamp(newPixelValue),
+                ]);
+            } else {
+                newImageHorizontal.setPixelXY(y, x, [oImageMatrix[x][y]]);
+            }
+        }
+    }
+
+    return {
+        imageVertical: newImageVertical.toDataURL(),
+        imageHorizontal: newImageHorizontal.toDataURL(),
+    };
 };
 
-const detector_roberts = async (imageBase64: string) => {
+export const detector_roberts = async (imageBase64: string) => {
+    let robertsMxHorizontal = [1, 0, 0, -1];
+    let robertsMyHorizontal = [0, 1, -1, 0];
+
+    let robertsMxVertical = [1, 0, -1, 0];
+    let robertsMyVertical = [0, -1, 0, 1];
+
     let oImage: Image = resizeImg(await Image.load(imageBase64)).grey();
-    let newImage: Image = new Image({
+    let newImageHorizontal: Image = new Image({
+        width: oImage.width,
+        height: oImage.height,
+        kind: ImageKind.GREY,
+    });
+
+    let newImageVertical: Image = new Image({
         width: oImage.width,
         height: oImage.height,
         kind: ImageKind.GREY,
@@ -230,10 +275,10 @@ const detector_roberts = async (imageBase64: string) => {
 
     const oImageMatrix: number[][] = getMatrixFromImage(oImage);
 
-    for (let x = 0; x < newImage.height; x++) {
-        for (let y = 0; y < newImage.width; y++) {
+    for (let x = 0; x < oImage.height; x++) {
+        for (let y = 0; y < oImage.width; y++) {
             if (ehBorda(x, y, oImage)) {
-                newImage.setPixelXY(y, x, [oImageMatrix[x][y]]);
+                newImageHorizontal.setPixelXY(y, x, [oImageMatrix[x][y]]);
             } else {
                 let PIXELlIST: number[] = [
                     oImageMatrix[x][y],
@@ -243,21 +288,51 @@ const detector_roberts = async (imageBase64: string) => {
                 ];
 
                 let Gx = PIXELlIST.reduce(
-                    (result, a, b) =>
-                        result + a * RobertSobelsOptions.RobertsMx[b],
+                    (result, a, b) => result + a * robertsMxHorizontal[b],
                     0
                 );
                 let Gy = PIXELlIST.reduce(
-                    (r, a, b) => r + a * RobertSobelsOptions.RobertsMy[b],
+                    (r, a, b) => r + a * robertsMyHorizontal[b],
                     0
                 );
 
-                let newPixelValue = Math.abs(Gx) + Math.abs(Gy);
+                let newPixelValue = 255 - (Math.abs(Gx) + Math.abs(Gy));
 
-                newImage.setPixelXY(y, x, [newPixelValue]);
+                newImageHorizontal.setPixelXY(y, x, [newPixelValue]);
             }
         }
     }
 
-    return newImage.toDataURL();
+    for (let x = 0; x < oImage.height; x++) {
+        for (let y = 0; y < oImage.width; y++) {
+            if (ehBorda(x, y, oImage)) {
+                newImageVertical.setPixelXY(y, x, [oImageMatrix[x][y]]);
+            } else {
+                let PIXELlIST: number[] = [
+                    oImageMatrix[x][y],
+                    oImageMatrix[x][y + 1],
+                    oImageMatrix[x + 1][y],
+                    oImageMatrix[x + 1][y + 1],
+                ];
+
+                let Gx = PIXELlIST.reduce(
+                    (result, a, b) => result + a * robertsMxVertical[b],
+                    0
+                );
+                let Gy = PIXELlIST.reduce(
+                    (r, a, b) => r + a * robertsMyVertical[b],
+                    0
+                );
+
+                let newPixelValue = 255 - (Math.abs(Gx) + Math.abs(Gy));
+
+                newImageVertical.setPixelXY(y, x, [newPixelValue]);
+            }
+        }
+    }
+
+    return {
+        horizontalImg: newImageHorizontal.toDataURL(),
+        verticalImg: newImageVertical.toDataURL(),
+    };
 };
